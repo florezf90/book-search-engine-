6eb3e61b9bf801506a2b610e78df9db0fbe3353a
florezf90 <florezf90@gmail.com>
2024-01-31 11:33:59 -0600

    fixed the mutations and the savedbook.jsx page component to make it functional based on the refactor done in the server folder for the typedefs and resolvers, page is fully functional now

--- client/src/pages/SavedBooks.jsx
+++ client/src/pages/SavedBooks.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/no-unknown-property */
-import { Container, Card, Button, Row } from "react-bootstrap";
+import { Container, Card, Button, Row, Col } from "react-bootstrap";
 
 import { useQuery, useMutation } from "@apollo/client";
 import { QUERY_ME } from "../utils/queries";
@@ -11,13 +11,14 @@ import Auth from "../utils/auth";
 import { removeBookId } from "../utils/localStorage";
 
 const SavedBooks = () => {
+  const { loading, data } = useQuery(QUERY_ME);
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
 
-const { loading, data} = useQuery(QUERY_ME);
-const [ removeBook, { error }] = useMutation(REMOVE_BOOK);
+  const userData = data?.me || {};
 
-const userData = data?.me || {};
-
+  // create function that accepts the book's mongo _id value as param and deletes the book from the database
   const handleDeleteBook = async (bookId) => {
+    // get token
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
     if (!token) {
@@ -29,7 +30,8 @@ const userData = data?.me || {};
         variables: { bookId },
       });
 
-    removeBookId(bookId);
+      // upon success, remove book's id from localStorage
+      removeBookId(bookId);
     } catch (err) {
       console.error(err);
     }
@@ -41,45 +43,48 @@ const userData = data?.me || {};
 
   return (
     <>
-      <div fluid className="text-light bg-dark">
+      <div fluid className="text-light bg-dark p-5">
         <Container>
-          <h1>Viewing {userData.username} books!</h1>
+          <h1>Viewing {userData.username}'s books!</h1>
         </Container>
       </div>
       <Container>
-        <h2>
+        <h2 className='pt-5'>
           {userData.savedBooks?.length
-            ? `Viewing ${userData.savedBooks.length} saved ${
-                userData.savedBooks.length === 1 ? "book" : "books"
-              }:`
-            : "You have no saved books!"}
+            ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'
+            }:`
+            : 'You have no saved books!'}
         </h2>
-        <Row>
-          {userData.savedBooks?.map((book) => {
-            return (
-              <Card key={book.bookId} border="dark">
-                {book.image ? (
-                  <Card.Img
-                    src={book.image}
-                    alt={`The cover for ${book.title}`}
-                    variant="top"
-                  />
-                ) : null}
-                <Card.Body>
-                  <Card.Title>{book.title}</Card.Title>
-                  <p className="small">Authors: {book.authors}</p>
-                  <Card.Text>{book.description}</Card.Text>
-                  <Button
-                    className="btn-block btn-danger"
-                    onClick={() => handleDeleteBook(book.bookId)}
-                  >
-                    Delete this Book!
-                  </Button>
-                </Card.Body>
-              </Card>
-            );
-          })}
-        </Row>
+        <div>
+          <Row>
+            {userData.savedBooks?.map((book) => {
+              return (
+                <Col md="4">
+                  <Card key={book.bookId} border="dark">
+                    {book.image ? (
+                      <Card.Img
+                        src={book.image}
+                        alt={`The cover for ${book.title}`}
+                        variant="top"
+                      />
+                    ) : null}
+                    <Card.Body>
+                      <Card.Title>{book.title}</Card.Title>
+                      <p className="small">Authors: {book.authors}</p>
+                      <Card.Text>{book.description}</Card.Text>
+                      <Button
+                        className="btn-block btn-danger"
+                        onClick={() => handleDeleteBook(book.bookId)}
+                      >
+                        Delete this Book!
+                      </Button>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              );
+            })}
+          </Row>
+        </div>
       </Container>
     </>
   );
