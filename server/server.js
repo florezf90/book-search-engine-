d35ed8902cd408440bb1013756f4430eca62586f
florezf90 <florezf90@gmail.com>
2024-01-31 11:08:50 -0600

    re created the server.js file to try to solve the 404 error when trying to reach /graphql

--- server/server.js
+++ server/server.js
@@ -1,38 +1,48 @@
-const express = require("express");
-const { ApolloServer } = require("apollo-server-express");
-const path = require("path");
-const { authMiddleware } = require("./utils/auth");
-const { typeDefs, resolvers } = require("./Schemas/index");
-const db = require("./config/connection");
-const { expressMiddleware } = require("@apollo/server/express4");
-const app = express();
+const express = require('express');
+const path = require('path');
+// Import the ApolloServer class
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+const { authMiddleware } = require('./utils/auth');
+
+// Import the two parts of a GraphQL schema
+const { typeDefs, resolvers } = require('./schemas');
+const db = require('./config/connection');
+
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
+const app = express();
 
-const StartApolloServer = async () => {
+// Create a new instance of an Apollo server with the GraphQL schema
+const startApolloServer = async () => {
   await server.start();
-
+  
   app.use(express.urlencoded({ extended: false }));
   app.use(express.json());
+  
+  app.use('/graphql', expressMiddleware(server, {
+    context: authMiddleware
+  }));
 
-  server.applyMiddleware({ app, path: "/graphql", cors: true }); // Serve the React app's entry point for all other routes
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
 
-  app.use(express.static(path.join(__dirname, "../client/dist")));
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../client/dist/index.html"));
-  });
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
 
-  db.once("open", () => {
+  db.once('open', () => {
     app.listen(PORT, () => {
       console.log(`API server running on port ${PORT}!`);
-      console.log(
-        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
-      );
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
     });
   });
 };
-StartApolloServer( );
+
+// Call the async function to start the server
+startApolloServer();
